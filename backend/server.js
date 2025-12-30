83939d16a8d7142f32117d0cef205b24d5a4e7c9
danieltabet333 <12131993@students.liu.edu.lb>
2025-12-30 19:44:35 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
